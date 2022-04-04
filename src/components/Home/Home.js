8b0067d8e46934fdcf9917c4c59dbf6ece1aced9
arifraj199 <karif9514@gmail.com>
2022-04-04 11:46:49 +0600

    home page product container added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import img from '../../images/camera-01.jpg';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is Home Page</h2>
+        <div className='products-container'>
+            <div className="title-container">
+                <h1>Find Best Camera From Us <br /> Build Your Goal on <span>Photography</span></h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aliquam, nam maxime soluta ex ab voluptates! Vero sed impedit esse? Sit temporibus sunt nemo, impedit commodi omnis voluptatibus sed obcaecati.</p>
+                <button>LIVE DEMO</button>
+            </div>
+            <div className="img-container">
+                <img src={img} alt="" />
+            </div>
         </div>
     );
 };
